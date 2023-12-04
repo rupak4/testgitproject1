971eb92355dad4a2889c087391552af25cc74f22
rupa <rupak4890@gmail.com>
2023-12-04 11:49:39 +0530

    1st commit

---
+++ src/test/java/testpackage/WelcomeTest.java
@@ -0,0 +1,12 @@
+package testpackage;
+
+public class WelcomeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("welcome");
+
+	}
+
+}
